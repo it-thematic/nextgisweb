2db9ecd79f6111a0166d6b4bdfb9fdbc1eef57e7
Pavel Shinyaev <shpawel@mail.ru>
2022-07-13 10:37:35 +0300

    приведение порта базы к строке для subprocess.Popen

--- nextgisweb/core/backup.py
+++ nextgisweb/core/backup.py
@@ -106,7 +106,7 @@ def pg_connection_options(env):
     con_args = env.core._db_connection_args()
     return [
         '--host', con_args['host'],
-        '--port', con_args['port'],
+        '--port', str(con_args['port']),
         '--username', con_args['username'],
         '--dbname', con_args['database'],
     ], con_args['password']
